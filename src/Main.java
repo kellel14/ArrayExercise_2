e168c9dd793ad066a377c1472ad71ef53589222d
kellel14 <kellel14@gmail.com>
2019-06-20 14:53:08 -0400

    initial push

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+
+//        String [] word = {"W","e","l","c","o","m","e"};
+        int word [] = {6};
+
+
+
+        for (int i = 0; i < word.length; i++) {
+
+            System.out.println(word[i]);
+        }
+        for (int i = (word.length - 1); i >= 0; i--){
+            System.out.println(word[i] +" ");
+        }
+
+    }
+}
